17e16d48a1e6eec3522fe2710d3782e002ceafa2
Mandeep <panesarmonika89@gmail.com>
2018-03-27 05:36:24 -0400

    first commit

---
+++ app/src/main/java/com/mandeepchatbasedapplication/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,26 @@
+package com.mandeepchatbasedapplication.flashchatnewfirebase;
+
+/**
+ * Created by admin on 3/26/2018.
+ */
+
+public class InstantMessage {
+    private String message;
+    private String author;
+
+    public InstantMessage(String message, String author){
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
